328ea82b3b77f645f5b4628f2738ac030315edec
Marden Melo <mardenmelo@gmail.com>
2020-01-08 01:11:37 -0300

    Projeto exceções concluído

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import java.util.Date;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Date now = new Date();
+		
+		System.out.println(now);
+	}
+
+}
